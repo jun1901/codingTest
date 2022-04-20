b1f402623b2152e0b1c7ff4037cf023d30ed9dcd
jun1901 <gkrwns1901@naver.com>
2022-04-20 19:47:17 +0900

    codingTest

--- src/swexpertacademy/level1/Problem.java
+++ src/swexpertacademy/level1/Problem.java
@@ -1,11 +1,13 @@
 package swexpertacademy.level1;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Problem {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
 
     }
 }
