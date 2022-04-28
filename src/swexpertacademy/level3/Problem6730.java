456aa360d467b71730052a5639535f59f461018c
jun1901 <gkrwns1901@naver.com>
2022-04-28 10:33:20 +0900

    codingTest

---
+++ src/swexpertacademy/level3/Problem6730.java
@@ -0,0 +1,17 @@
+package swexpertacademy.level3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem6730 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= num ; i++) {
+
+        }
+
+    }
+}
