8cd6dfe2cb6019c378c9e3b56bedf18b47d15c5a
jun1901 <gkrwns1901@naver.com>
2022-05-09 22:32:00 +0900

    codingTest

--- src/swexpertacademy/level3/Problem8016.java
+++ src/swexpertacademy/level3/Problem8016.java
@@ -1,17 +1,19 @@
 package swexpertacademy.level3;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Problem {
+public class Problem8016 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuffer sb = new StringBuffer();
         int T = Integer.parseInt(br.readLine());
 
         for (int i = 1; i <= T ; i++) {
-
-            sb.append("#"+i+" "+"   "+"\n");
+            long N = Long.parseLong(br.readLine());
+            sb.append("#"+i+" "+(1+2*Math.pow(N-1,2))+" "+(2*Math.pow(N,2)-1)+"\n");
+            //sb.append("#"+i+" "+(1+2*(N-1)*(N-1))+" "+(2*N*N-1)+"\n");
         }
         System.out.print(sb);
     }
