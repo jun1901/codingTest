758ad5cfb2cd99da01872c6d0bb44b8518b059e6
jun1901 <gkrwns1901@naver.com>
2022-05-09 10:02:13 +0900

    codingTest

---
+++ src/swexpertacademy/level3/R_Problem1493.java
@@ -0,0 +1,21 @@
+package swexpertacademy.level3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class R_Problem1493 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= T ; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int p = Integer.parseInt(st.nextToken()), q = Integer.parseInt(st.nextToken());
+            int [][] num = new int[q][q];
+
+        }
+
+    }
+}
