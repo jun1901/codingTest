d29c0f080fd28f58466abacfd01da7c9a50525aa
jun1901 <gkrwns1901@naver.com>
2022-05-12 10:05:45 +0900

    codingTest

--- src/swexpertacademy/level2/Problem.java
+++ src/swexpertacademy/level2/Problem.java
@@ -1,18 +1,18 @@
 package swexpertacademy.level2;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
 
 public class Problem {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int num = Integer.parseInt(br.readLine());
+        StringBuffer sb = new StringBuffer();
+        int T = Integer.parseInt(br.readLine());
 
-        for (int i = 1; i <= num ; i++) {
+        for (int i = 1; i <= T ; i++) {
 
+            sb.append("#"+i+" "+"   "+"\n");
         }
-
+        System.out.print(sb);
     }
 }
