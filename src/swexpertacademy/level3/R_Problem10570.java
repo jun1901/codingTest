b11f133406e449e191454da7030d339c85441bca
jun1901 <gkrwns1901@naver.com>
2022-05-16 12:33:15 +0900

    codingTest

--- src/swexpertacademy/level3/R_Problem10570.java
+++ src/swexpertacademy/level3/R_Problem10570.java
@@ -8,34 +8,28 @@ import java.util.StringTokenizer;
 public class R_Problem10570 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer(), rs;
         int num = Integer.parseInt(br.readLine());
-        boolean sw = true;
 
         for (int i = 1; i <= num ; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int A = Integer.parseInt(st.nextToken()),
-                B = Integer.parseInt(st.nextToken()),
-                result = 0;
-            double min = Math.sqrt(A), max = Math.sqrt(B);
-            min = min == (int) min ? min : (int) min + 1;
+            int A = Integer.parseInt(st.nextToken()), B = Integer.parseInt(st.nextToken()), result = 0;
+            int min = (int)Math.ceil(Math.sqrt(A)), max = (int)Math.sqrt(B);
 
-            System.out.println(min + ", " + max);
-
-            for (int j = (int)min; j <= (int)max ; j++) {
-                String strNum = j + "";
-                System.out.println(strNum);
-                sw = true;
-                for (int k = 0; k < strNum.length() / 2; k++) {
-                    if (strNum.charAt(k) != strNum.charAt(strNum.length() - k - 1)) {
-                        sw = false;
-                        break;
-                    }
+            for (int j = min; j <= max ; j++) {
+                String s = j+"";
+                rs = new StringBuffer(s);
+                if(s.contentEquals(rs.reverse())){
+                    s = (int)Math.pow(j,2)+"";
+                    rs = new StringBuffer(s);
+                    if(s.contentEquals(rs.reverse())) result++;
                 }
-                result = sw ? result +1 : result;
             }
-            System.out.printf("#%d %d\n", i, result);
+            sb.append("#"+i+" "+result+"\n");
+            //System.out.printf("#%d %d\n", i, result);
 
         }
+        System.out.println(sb);
 
     }
 }
