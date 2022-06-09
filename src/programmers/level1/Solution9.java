53bcaa80170b5819901ce60f3fc7740b0ca768bc
jun1901 <gkrwns1901@naver.com>
2022-06-09 23:01:10 +0900

    codingTest

---
+++ src/programmers/level1/Solution9.java
@@ -0,0 +1,18 @@
+package programmers.level1;
+
+public class Solution9 { // 내적 - https://programmers.co.kr/learn/courses/30/lessons/70128?language=java
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4}, b = {-3,-1,0,2};
+        System.out.println(solution(a,b));
+    }
+
+    public static int solution(int[] a, int[] b) {
+        int answer = 0;
+        for (int i = 0; i < a.length; i++) answer += a[i]*b[i];
+
+
+        return answer;
+    }
+
+
+}
