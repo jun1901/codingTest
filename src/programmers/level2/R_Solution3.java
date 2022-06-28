ba9b5d1b10fd17f113425c4b67258bbccb4d33bf
HakJun <gkrwns1901@naver.com>
2022-06-28 21:46:28 +0900

    codingTest

---
+++ src/programmers/level2/R_Solution3.java
@@ -0,0 +1,18 @@
+package programmers.level2;
+
+public class R_Solution3 { // 카카오프렌즈 컬러링북 - https://programmers.co.kr/learn/courses/30/lessons/1829?language=java
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] solution(int m, int n, int[][] picture) {
+        int numberOfArea = 0;
+        int maxSizeOfOneArea = 0;
+
+        int[] answer = new int[2];
+        answer[0] = numberOfArea;
+        answer[1] = maxSizeOfOneArea;
+        return answer;
+    }
+
+}
