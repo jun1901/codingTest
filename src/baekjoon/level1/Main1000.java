2fbcde5d314943afdfa0942993a90b38390de669
HakJun <gkrwns1901@naver.com>
2022-07-01 11:13:03 +0900

    codingTest

---
+++ src/baekjoon/level1/Main1000.java
@@ -0,0 +1,13 @@
+package baekjoon.level1;
+
+import java.io.*;
+import java.util.*;
+
+public class Main1000 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken()), B = Integer.parseInt(st.nextToken());
+        System.out.println(A + B);
+    }
+}
